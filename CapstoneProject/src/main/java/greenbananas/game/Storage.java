2e6d3635486d164d04b48360befecc9ef549beed
rohanparikh5 <rparikh931@student.fuhsd.org>
2022-05-11 13:04:23 -0700

    update

--- CapstoneProject/src/main/java/greenbananas/game/Storage.java
+++ CapstoneProject/src/main/java/greenbananas/game/Storage.java
@@ -1,9 +1,13 @@
 package greenbananas.game;
 
 public class Storage {
+	private int x;
+	private int y;
 	
-	public Storage(){
-		
+	public Storage(int x, int y){
+		this.x=x;
+		this.y=y;
 	}
+	
 
 }
